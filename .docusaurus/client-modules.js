9aa3047b5bfbd6af9af6247b005f81386703e722
Christian Legnitto <christian@legnitto.com>
2024-10-30 11:21:31 -0400

    Remove docusaurus build dir

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/legnitto/src/rust-gpu.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/legnitto/src/rust-gpu.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/legnitto/src/rust-gpu.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/legnitto/src/rust-gpu.github.io/src/css/custom.css"),
-];
