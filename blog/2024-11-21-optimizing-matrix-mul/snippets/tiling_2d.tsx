ffdbca09df7f3bd4a50270b8250ed245f1b620c9
Christian Legnitto <LegNeato@users.noreply.github.com>
2024-11-21 16:02:40 -0400

    Remove more "simd" label (#36)
    Missed some previously

--- blog/2024-11-21-optimizing-matrix-mul/snippets/tiling_2d.tsx
+++ blog/2024-11-21-optimizing-matrix-mul/snippets/tiling_2d.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Snippet from "@site/src/components/Snippet";
 import RustKernelSource from "!!raw-loader!../code/crates/gpu/tiling_2d/src/lib.rs";
 
-export const RustTiling2dSimd: React.FC = () => (
+export const RustTiling2d: React.FC = () => (
     <Snippet language="rust" className="text-xs" title="2D tiling kernel with Rust GPU">
       {RustKernelSource}
     </Snippet>
