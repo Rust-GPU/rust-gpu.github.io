ede2a7c88f8f8159296181d2b2c8c289ce6fc2e7
Christian Legnitto <christian@legnitto.com>
2025-03-26 10:34:40 -0400

    Add more sizes to benchmarks and load config from env

--- blog/2024-11-25-optimizing-matmul/code/benches/gpu_bench.rs
+++ blog/2024-11-25-optimizing-matmul/code/benches/gpu_bench.rs
@@ -13,7 +13,9 @@ const SIZES: &[(u32, u32, u32)] = &[
     // Square matrices
     (2, 2, 2),
     (4, 4, 4),
+    (8, 8, 4),
     (16, 16, 16),
+    (32, 32, 32),
     (64, 64, 64),
     (128, 128, 128),
     (256, 256, 256),
@@ -143,7 +145,8 @@ criterion_group! {
     config = Criterion::default()
         .with_plots()
         .significance_level(0.01)
-        .noise_threshold(0.02);
+        .noise_threshold(0.02)
+        .configure_from_args();
     targets = bench_all_variants
 }
 
