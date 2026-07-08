2701c19f0906f6b4e379d6916ddfb88de0cbdb7c
firestar99 <firestar99@sydow.cloud>
2026-07-08 11:05:09 +0200

    fix frontpage example

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              yarn
+            ];
+          };
+      });
+}
